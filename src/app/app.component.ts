62b92f8ffe5ed02aca4a18e40a441b4c689f8646
Rosber Almeida Junior <rosber.almeida@autotrac.corp>
2017-10-09 19:32:59 -0300

    versao 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+valor1: number;
+valor2: number;
+cor1: string;
+cor2: string;
+
+ngOnInit() {
+  this.valor1 = Math.round((Math.random() * 100) + 1);
+  this.valor2 = Math.round((Math.random() * 100) + 1);
+
+  this.cor1 = (this.valor1 > 50) ? 'bg-success' : 'bg-danger';
+  this.cor2 = (this.valor2 > 50) ? 'bg-success' : 'bg-danger';
+
+
+}
+
+
 }
