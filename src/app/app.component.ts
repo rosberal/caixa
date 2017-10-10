779b6713a31a8d0f484a0b0bc406105c1a9c87f2
Rosber Almeida Junior <rosber.almeida@autotrac.corp>
2017-10-10 07:56:29 -0300

    ver 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,13 +12,14 @@ valor2: number;
 cor1: string;
 cor2: string;
 
+datahora: number;
 ngOnInit() {
   this.valor1 = Math.round((Math.random() * 100) + 1);
   this.valor2 = Math.round((Math.random() * 100) + 1);
 
   this.cor1 = (this.valor1 > 50) ? 'bg-success' : 'bg-danger';
   this.cor2 = (this.valor2 > 50) ? 'bg-success' : 'bg-danger';
-
+this.datahora = Date.now();
 
 }
 
